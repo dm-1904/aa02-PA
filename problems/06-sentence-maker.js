1106aee17f63ba5fbf087cc996c040ca2401ee7e
damonryon <damon.ryon@gmail.com>
2024-04-07 15:46:38 -0700

    06 completed

--- problems/06-sentence-maker.js
+++ problems/06-sentence-maker.js
@@ -11,8 +11,13 @@ sentenceMaker('I', 'love', 'coding'); // 'I love coding!'
 sentenceMaker('The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'); // 'The quick brown fox jumps over the lazy dog!'
 ***********************************************************************/
 
-// Your code here 
+const sentenceMaker = (...str) => {
+	return (str.join(' ') + '!')
+}
 
+sentenceMaker('Hello', 'World'); // 'Hello World!'
+sentenceMaker('I', 'love', 'coding'); // 'I love coding!'
+sentenceMaker('The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'); // 'The quick brown fox jumps over the lazy dog!'
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
 	module.exports = sentenceMaker;
