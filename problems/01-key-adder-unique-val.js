ab89e273def17bfd4a5befe4249f0101cb9b5d7e
damonryon <damon.ryon@gmail.com>
2024-04-07 12:50:00 -0700

    01 solved

--- problems/01-key-adder-unique-val.js
+++ problems/01-key-adder-unique-val.js
@@ -14,8 +14,15 @@ console.log(cat); // { name: "Willie", color: "orange", toy: "yarn" }
 ***********************************************************************/
 
 function keyAdderUniqueVal(object, key, value) {
-	// Your code here 
+	let values = Object.values(object)
+	if(!values.includes(value)) object[key] = value
+	return object 
 }
 
+
+let cat = { name: 'Willie', color: 'orange' };
+keyAdderUniqueVal(cat, "toy", "yarn"); // => {name: "Willie", color: "orange", toy: "yarn"}
+keyAdderUniqueVal(cat, "fruit", "orange"); // => {name: "Willie", color: "orange", toy: "yarn"}
+console.log(cat); // { name: "Willie", color: "orange", toy: "yarn" }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyAdderUniqueVal;
