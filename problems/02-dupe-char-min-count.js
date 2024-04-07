423bb8b028abc9459b7212b4372f8cb93843376d
damonryon <damon.ryon@gmail.com>
2024-04-07 13:43:27 -0700

    02 solved

--- problems/02-dupe-char-min-count.js
+++ problems/02-dupe-char-min-count.js
@@ -12,12 +12,31 @@ duplicateCharMinCount("What about a longer string?", 3) // ["a", "t", " "]
 ***********************************************************************/
 
 function countCharacters(string) {
-	// Your code here 
+	let arr = string.split('')
+	let obj = {}
+
+	for(let el of arr){
+		if(obj[el]) obj[el]++
+		else obj[el] = 1
+	}
+	return obj
 }
 
 function duplicateCharMinCount(string, minCount) {
-	// Your code here 
+	let count = countCharacters(string)
+	return Object.keys(countCharacters(string)).filter(key => count [key] >= minCount)
 }
+// 	let arr = []
+// 	let count = countCharacters(string)
 
+// 	for(let key in count){
+// 		if(count[key] >= minCount) arr.push(key)
+// 	}
+// 	console.log(arr)
+// 	return arr
+// }
+duplicateCharMinCount("apple", 2) // ["p"]
+duplicateCharMinCount("banana", 2) // ["a", "n"]
+duplicateCharMinCount("What about a longer string?", 3) // ["a", "t", " "]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = duplicateCharMinCount;
